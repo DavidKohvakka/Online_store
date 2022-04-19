f9a955358c3d182811072fabf039e2e62cb26d92
David Kohvakka <david.kohvakka@live.com>
2022-04-19 20:41:50 +0300

    products page and logic

--- routes/controller/app/products.js
+++ routes/controller/app/products.js
@@ -1,10 +1,15 @@
 const Categories = require('../../../model/category')
+const Product = require('../../../model/product')
 
 module.exports = async(req, res) => {
     try{
-        const categories = await Categories.find()
+        const categories = await Categories.find();
+        const products = await Product.find();
+        console.log(products)
+        const uniqueProducts = Array.from(new Set(products))
+         
         console.log(categories)
-        res.render('pages/products', {categories: categories})
+        res.render('pages/products', {categories: categories, products: products})
     }
     catch (err) {
         console.log(err)
